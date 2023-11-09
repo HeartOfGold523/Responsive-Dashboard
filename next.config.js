7fb98f78436f1c1954984bd2c04faf888cd3723e
HeartOfGold523 <ashconabae@gmail.com>
2023-11-09 00:46:16 -0500

    Removed next export, instead output: export in next.config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 const isGithubActions = process.env.GITHUB_ACTIONS || false;
 
+let output = undefined;
 let assetPrefix = "";
 let basePath = "/";
 
@@ -7,12 +8,14 @@ if (isGithubActions) {
   // trim off `<owner>/`
   const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
 
+  output = "export";
   assetPrefix = `/${repo}/`;
   basePath = `/${repo}`;
 }
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output,
   assetPrefix,
   basePath,
 };
