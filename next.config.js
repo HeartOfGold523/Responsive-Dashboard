9ac667a52245be770e8eff538b20311873e74e2f
HeartOfGold523 <ashconabae@gmail.com>
2023-11-09 09:38:22 -0500

    remove gh-actions/gh-pages. deploying to vercel instead

--- next.config.js
+++ next.config.js
@@ -1,23 +1,4 @@
-const isGithubActions = process.env.GITHUB_ACTIONS || false;
-
-let output = undefined;
-let assetPrefix = "";
-let basePath = "";
-
-if (isGithubActions) {
-  // trim off `<owner>/`
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
-
-  output = "export";
-  assetPrefix = `/${repo}/`;
-  basePath = `/${repo}`;
-}
-
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output,
-  assetPrefix,
-  basePath,
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
